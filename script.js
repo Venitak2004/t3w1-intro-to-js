796944fada44cf73f4e4a531d3bbe83ecce3b93c
Venita Kissell <venitak@hotmail.com>
2024-10-18 20:22:40 +1000

    basics of javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello World, what is happening in the world");
+
+
+//x = 5;//global variable, not good syntax
+
+//var x = 5; //avoid not popular
+
+let x = 5;//used mostly
+
+const x = 5;//good to use for a value that you can't change.
